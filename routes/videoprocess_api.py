f7165bdeff86205a4caa6d03f581f4a002473925
messi9999 <micklepanda812@gmail.com>
2025-02-25 07:57:34 -0800

    udpated

--- routes/videoprocess_api.py
+++ routes/videoprocess_api.py
@@ -207,7 +207,7 @@ async def upload_images(files: List[UploadFile] = File(...)):
         image_paths.append(file_path)
     
     api_key = os.getenv("API_KEY")
-    prompt = "I uploaded the screenshot of basketball playing and shooting. Please give me advices to upgrade player's skills."
+    prompt = "I attached the screenshots of my weightlifting forms. At the first please check the images very carefully and give me advice what is wrong in forms."
     
     response = utils.send_image_to_gpt4(api_key, image_paths, prompt)
     return {"response": response}
