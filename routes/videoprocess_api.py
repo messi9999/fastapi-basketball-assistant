34d3f0e155a179bf5540a07500791e7e45767d06
messi9999 <micklepanda812@gmail.com>
2025-02-25 08:06:58 -0800

    commented it

--- routes/videoprocess_api.py
+++ routes/videoprocess_api.py
@@ -5,7 +5,7 @@ import os
 import shutil
 import multiprocessing
 from pathlib import Path
-from utils.utils import process_video_with_yolo, process_video_with_yolo_and_pose
+# from utils.utils import process_video_with_yolo, process_video_with_yolo_and_pose
 import re
 
 from dotenv import load_dotenv
@@ -24,174 +24,174 @@ router = APIRouter()
 # Global dictionary to store queues by process ID
 process_queues = {}
 
-# Function to wrap the call to process_video_with_yolo
-def task_wrapper(video_path, queue, target_class=[0]):
-    try:
-        output_video_path = process_video_with_yolo(video_path, target_class)
-        filename_with_extension = os.path.basename(output_video_path)
+# # Function to wrap the call to process_video_with_yolo
+# def task_wrapper(video_path, queue, target_class=[0]):
+#     try:
+#         output_video_path = process_video_with_yolo(video_path, target_class)
+#         filename_with_extension = os.path.basename(output_video_path)
         
         
-        queue.put({"status": "completed", "result": filename_with_extension})
-    except Exception as e:
-        queue.put({"status": "error", "result": str(e)})
-# Function to wrap the call to process_video_with_yolo
-def task_wrapper2(video_path, queue, target_class=[3]):
-    try:
-        output_video_path = process_video_with_yolo_and_pose(video_path, target_class)
-        filename_with_extension = os.path.basename(output_video_path)
+#         queue.put({"status": "completed", "result": filename_with_extension})
+#     except Exception as e:
+#         queue.put({"status": "error", "result": str(e)})
+# # Function to wrap the call to process_video_with_yolo
+# def task_wrapper2(video_path, queue, target_class=[3]):
+#     try:
+#         output_video_path = process_video_with_yolo_and_pose(video_path, target_class)
+#         filename_with_extension = os.path.basename(output_video_path)
         
         
-        queue.put({"status": "completed", "result": filename_with_extension})
-    except Exception as e:
-        queue.put({"status": "error", "result": str(e)})
+#         queue.put({"status": "completed", "result": filename_with_extension})
+#     except Exception as e:
+#         queue.put({"status": "error", "result": str(e)})
 
 
-@router.post("/create-process-player")
-async def create_process_player(file: UploadFile = File(...)):
-    print("File Downloading...")
-    random_string = str(uuid.uuid4())
-    base_name, extension = os.path.splitext(file.filename)
-    new_file_name = f"{base_name}_{random_string}{extension}"
-    file_location = f"videos/source/{new_file_name}"
+# @router.post("/create-process-player")
+# async def create_process_player(file: UploadFile = File(...)):
+#     print("File Downloading...")
+#     random_string = str(uuid.uuid4())
+#     base_name, extension = os.path.splitext(file.filename)
+#     new_file_name = f"{base_name}_{random_string}{extension}"
+#     file_location = f"videos/source/{new_file_name}"
     
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    print("Processing...")
-    # Create a Queue to receive the output video path from the task
-    queue = multiprocessing.Queue()
+#     with open(file_location, "wb") as buffer:
+#         shutil.copyfileobj(file.file, buffer)
+#     print("Processing...")
+#     # Create a Queue to receive the output video path from the task
+#     queue = multiprocessing.Queue()
 
-    target_class = [3]
-    # Create a Process that will execute the task_wrapper function
-    process = multiprocessing.Process(target=task_wrapper, args=(file_location, queue, target_class))
+#     target_class = [3]
+#     # Create a Process that will execute the task_wrapper function
+#     process = multiprocessing.Process(target=task_wrapper, args=(file_location, queue, target_class))
 
-    # Start the process
-    process.start()
+#     # Start the process
+#     process.start()
 
-    # Store the queue in the global dictionary using the process ID
-    process_queues[process.pid] = queue
+#     # Store the queue in the global dictionary using the process ID
+#     process_queues[process.pid] = queue
 
-    # Return the process ID (task ID)
-    return {"task_id": process.pid}
+#     # Return the process ID (task ID)
+#     return {"task_id": process.pid}
 
 
-@router.post("/create-process-ball")
-async def create_process_ball(file: UploadFile = File(...)):
-    print("File Downloading...")
-    random_string = str(uuid.uuid4())
-    base_name, extension = os.path.splitext(file.filename)
-    new_file_name = f"{base_name}_{random_string}{extension}"
-    file_location = f"videos/source/{new_file_name}"
+# @router.post("/create-process-ball")
+# async def create_process_ball(file: UploadFile = File(...)):
+#     print("File Downloading...")
+#     random_string = str(uuid.uuid4())
+#     base_name, extension = os.path.splitext(file.filename)
+#     new_file_name = f"{base_name}_{random_string}{extension}"
+#     file_location = f"videos/source/{new_file_name}"
     
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    print("Processing...")
-    # Create a Queue to receive the output video path from the task
-    queue = multiprocessing.Queue()
+#     with open(file_location, "wb") as buffer:
+#         shutil.copyfileobj(file.file, buffer)
+#     print("Processing...")
+#     # Create a Queue to receive the output video path from the task
+#     queue = multiprocessing.Queue()
     
-    target_class = [0]
+#     target_class = [0]
 
-    # Create a Process that will execute the task_wrapper function
-    process = multiprocessing.Process(target=task_wrapper, args=(file_location, queue, target_class))
+#     # Create a Process that will execute the task_wrapper function
+#     process = multiprocessing.Process(target=task_wrapper, args=(file_location, queue, target_class))
 
-    # Start the process
-    process.start()
+#     # Start the process
+#     process.start()
 
-    # Store the queue in the global dictionary using the process ID
-    process_queues[process.pid] = queue
+#     # Store the queue in the global dictionary using the process ID
+#     process_queues[process.pid] = queue
 
-    # Return the process ID (task ID)
-    return {"task_id": process.pid}
+#     # Return the process ID (task ID)
+#     return {"task_id": process.pid}
 
-@router.post("/create-process-pose")
-async def create_process_pose(file: UploadFile = File(...)):
-    print("File Downloading...")
-    random_string = str(uuid.uuid4())
-    base_name, extension = os.path.splitext(file.filename)
-    new_file_name = f"{base_name}_{random_string}{extension}"
-    file_location = f"videos/source/{new_file_name}"
+# @router.post("/create-process-pose")
+# async def create_process_pose(file: UploadFile = File(...)):
+#     print("File Downloading...")
+#     random_string = str(uuid.uuid4())
+#     base_name, extension = os.path.splitext(file.filename)
+#     new_file_name = f"{base_name}_{random_string}{extension}"
+#     file_location = f"videos/source/{new_file_name}"
     
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    print("Processing...")
-    # Create a Queue to receive the output video path from the task
-    queue = multiprocessing.Queue()
+#     with open(file_location, "wb") as buffer:
+#         shutil.copyfileobj(file.file, buffer)
+#     print("Processing...")
+#     # Create a Queue to receive the output video path from the task
+#     queue = multiprocessing.Queue()
     
-    target_class = [3]
+#     target_class = [3]
 
-    # Create a Process that will execute the task_wrapper function
-    process = multiprocessing.Process(target=task_wrapper2, args=(file_location, queue, target_class))
+#     # Create a Process that will execute the task_wrapper function
+#     process = multiprocessing.Process(target=task_wrapper2, args=(file_location, queue, target_class))
 
-    # Start the process
-    process.start()
+#     # Start the process
+#     process.start()
 
-    # Store the queue in the global dictionary using the process ID
-    process_queues[process.pid] = queue
+#     # Store the queue in the global dictionary using the process ID
+#     process_queues[process.pid] = queue
 
-    # Return the process ID (task ID)
-    return {"task_id": process.pid}
-
-
-@router.get("/get-result/{task_id}")
-async def get_result(task_id: int):
-    # Convert task_id to int, as it will be received as a string from the path
-    task_id = int(task_id)
-
-    # Check if the task_id is in the global dictionary
-    if task_id in process_queues:
-        queue = process_queues[task_id]
-        try:
-            # Non-blocking get from the queue with a timeout
-            result = queue.get_nowait()
-            return result
-        except multiprocessing.queues.Empty:
-            # If the queue is empty, the task is still running
-            return {"status": "running", "result": task_id}
-    else:
-        raise HTTPException(status_code=404, detail="Task ID not found")
+#     # Return the process ID (task ID)
+#     return {"task_id": process.pid}
 
 
+# @router.get("/get-result/{task_id}")
+# async def get_result(task_id: int):
+#     # Convert task_id to int, as it will be received as a string from the path
+#     task_id = int(task_id)
 
-@router.get("/download-video/{filename}")
-async def download_video(filename: str):
-    video_path = Path(f"videos/result/{filename}")
-
-    if not video_path.is_file():
-        raise HTTPException(status_code=404, detail="Video not found")
-
-    # return FileResponse(path=video_path, filename=filename, media_type='application/octet-stream',  headers={"Content-Disposition": "attachment; filename=video.mp4"})
-    return FileResponse(path=video_path,  headers={"Content-Disposition": f"attachment; filename={filename}"})
+#     # Check if the task_id is in the global dictionary
+#     if task_id in process_queues:
+#         queue = process_queues[task_id]
+#         try:
+#             # Non-blocking get from the queue with a timeout
+#             result = queue.get_nowait()
+#             return result
+#         except multiprocessing.queues.Empty:
+#             # If the queue is empty, the task is still running
+#             return {"status": "running", "result": task_id}
+#     else:
+#         raise HTTPException(status_code=404, detail="Task ID not found")
 
 
 
-@router.get("/stream/{filename}")
-def stream_video(filename: str, range: str = None):
-    video_path = Path(f"videos/result/{filename}")
-    if not video_path.exists():
-        return {"error": "Video not found"}
+# @router.get("/download-video/{filename}")
+# async def download_video(filename: str):
+#     video_path = Path(f"videos/result/{filename}")
 
-    file_size = video_path.stat().st_size
-    start = 0
-    end = file_size - 1
+#     if not video_path.is_file():
+#         raise HTTPException(status_code=404, detail="Video not found")
 
-    if range:
-        match = re.match(r"bytes=(\d+)-(\d*)", range)
-        if match:
-            start = int(match.group(1))
-            if match.group(2):
-                end = int(match.group(2))
+#     # return FileResponse(path=video_path, filename=filename, media_type='application/octet-stream',  headers={"Content-Disposition": "attachment; filename=video.mp4"})
+#     return FileResponse(path=video_path,  headers={"Content-Disposition": f"attachment; filename={filename}"})
 
-    chunk_size = (end - start) + 1
-    with open(video_path, "rb") as f:
-        f.seek(start)
-        data = f.read(chunk_size)
 
-    headers = {
-        "Content-Range": f"bytes {start}-{end}/{file_size}",
-        "Accept-Ranges": "bytes",
-        "Content-Length": str(chunk_size),
-    }
 
-    return Response(data, status_code=206, headers=headers, media_type="video/mp4")
+# @router.get("/stream/{filename}")
+# def stream_video(filename: str, range: str = None):
+#     video_path = Path(f"videos/result/{filename}")
+#     if not video_path.exists():
+#         return {"error": "Video not found"}
+
+#     file_size = video_path.stat().st_size
+#     start = 0
+#     end = file_size - 1
+
+#     if range:
+#         match = re.match(r"bytes=(\d+)-(\d*)", range)
+#         if match:
+#             start = int(match.group(1))
+#             if match.group(2):
+#                 end = int(match.group(2))
+
+#     chunk_size = (end - start) + 1
+#     with open(video_path, "rb") as f:
+#         f.seek(start)
+#         data = f.read(chunk_size)
+
+#     headers = {
+#         "Content-Range": f"bytes {start}-{end}/{file_size}",
+#         "Accept-Ranges": "bytes",
+#         "Content-Length": str(chunk_size),
+#     }
+
+#     return Response(data, status_code=206, headers=headers, media_type="video/mp4")
 
 
 @router.post("/upload")
--- utils/utils.py
+++ utils/utils.py
@@ -8,169 +8,169 @@ import mediapipe as mp
 import base64
 import requests
 
-def process_video_with_yolo_and_pose(video_path, target_class=[3]):
-    # Load the YOLO model
-    model = YOLO("models/best1.pt")
+# def process_video_with_yolo_and_pose(video_path, target_class=[3]):
+#     # Load the YOLO model
+#     model = YOLO("models/best1.pt")
 
-    # Initialize MediaPipe Pose
-    mp_pose = mp.solutions.pose
-    mp_drawing = mp.solutions.drawing_utils
-    mp_drawing_styles = mp.solutions.drawing_styles
-    pose_estimator = mp_pose.Pose()
+#     # Initialize MediaPipe Pose
+#     mp_pose = mp.solutions.pose
+#     mp_drawing = mp.solutions.drawing_utils
+#     mp_drawing_styles = mp.solutions.drawing_styles
+#     pose_estimator = mp_pose.Pose()
 
-    # Open the video file
-    cap = cv2.VideoCapture(video_path)
+#     # Open the video file
+#     cap = cv2.VideoCapture(video_path)
 
-    # Get video properties for the output video
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
+#     # Get video properties for the output video
+#     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+#     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#     fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-    # Set up FFMPEG process for writing the video
-    file_name_with_extension = os.path.basename(video_path)
-    file_name, file_extension = os.path.splitext(file_name_with_extension)
-    output_video_path = f"videos/result/{file_name}_output.mp4"
+#     # Set up FFMPEG process for writing the video
+#     file_name_with_extension = os.path.basename(video_path)
+#     file_name, file_extension = os.path.splitext(file_name_with_extension)
+#     output_video_path = f"videos/result/{file_name}_output.mp4"
 
-    ffmpeg_cmd = [
-        'ffmpeg', '-y',  # Overwrite if file exists
-        '-f', 'rawvideo', '-vcodec', 'rawvideo', '-pix_fmt', 'bgr24',
-        '-s', f'{frame_width}x{frame_height}', '-r', str(fps),
-        '-i', '-', '-c:v', 'libx264', '-preset', 'fast', '-pix_fmt', 'yuv420p',
-        output_video_path
-    ]
-    ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
+#     ffmpeg_cmd = [
+#         'ffmpeg', '-y',  # Overwrite if file exists
+#         '-f', 'rawvideo', '-vcodec', 'rawvideo', '-pix_fmt', 'bgr24',
+#         '-s', f'{frame_width}x{frame_height}', '-r', str(fps),
+#         '-i', '-', '-c:v', 'libx264', '-preset', 'fast', '-pix_fmt', 'yuv420p',
+#         output_video_path
+#     ]
+#     ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
 
-    # Loop through the video frames
-    while cap.isOpened():
-        # Read a frame from the video
-        success, frame = cap.read()
+#     # Loop through the video frames
+#     while cap.isOpened():
+#         # Read a frame from the video
+#         success, frame = cap.read()
 
-        if success:
-            # Run YOLO inference on the frame
-            results = model(frame)
+#         if success:
+#             # Run YOLO inference on the frame
+#             results = model(frame)
 
-            names = results[0].names
-            boxes = results[0].boxes
-            xyxys = boxes.xyxy
-            cls = boxes.cls
+#             names = results[0].names
+#             boxes = results[0].boxes
+#             xyxys = boxes.xyxy
+#             cls = boxes.cls
 
-            for cla, xyxy in zip(cls, xyxys):
-                cla_key = int(cla.item())
-                tensor_list = xyxy.tolist()
-                int_list = [int(element) for element in tensor_list]
+#             for cla, xyxy in zip(cls, xyxys):
+#                 cla_key = int(cla.item())
+#                 tensor_list = xyxy.tolist()
+#                 int_list = [int(element) for element in tensor_list]
 
-                # Check if the detection class matches target_class
-                if cla_key in target_class:
-                    # Extract bounding box coordinates
-                    x1, y1, x2, y2 = int_list
-                    label = names[cla_key]
+#                 # Check if the detection class matches target_class
+#                 if cla_key in target_class:
+#                     # Extract bounding box coordinates
+#                     x1, y1, x2, y2 = int_list
+#                     label = names[cla_key]
 
-                    # Crop the detected player region
-                    player_crop = frame[y1:y2, x1:x2]
+#                     # Crop the detected player region
+#                     player_crop = frame[y1:y2, x1:x2]
 
-                    # Run pose estimation on the cropped region
-                    pose_results = pose_estimator.process(cv2.cvtColor(player_crop, cv2.COLOR_BGR2RGB))
+#                     # Run pose estimation on the cropped region
+#                     pose_results = pose_estimator.process(cv2.cvtColor(player_crop, cv2.COLOR_BGR2RGB))
 
-                    # Draw the bounding box and label on the frame
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                    cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+#                     # Draw the bounding box and label on the frame
+#                     cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+#                     cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
 
-                    # If pose landmarks are detected, overlay them on the original frame
-                    if pose_results.pose_landmarks:
-                        # Draw pose landmarks and connections
-                        mp_drawing.draw_landmarks(
-                            frame[y1:y2, x1:x2],
-                            pose_results.pose_landmarks,
-                            mp_pose.POSE_CONNECTIONS,
-                            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-                        )
+#                     # If pose landmarks are detected, overlay them on the original frame
+#                     if pose_results.pose_landmarks:
+#                         # Draw pose landmarks and connections
+#                         mp_drawing.draw_landmarks(
+#                             frame[y1:y2, x1:x2],
+#                             pose_results.pose_landmarks,
+#                             mp_pose.POSE_CONNECTIONS,
+#                             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
+#                         )
 
-            # Write the processed frame to FFMPEG
-            ffmpeg_process.stdin.write(frame.tobytes())
-        else:
-            # Break the loop if the end of the video is reached
-            break
+#             # Write the processed frame to FFMPEG
+#             ffmpeg_process.stdin.write(frame.tobytes())
+#         else:
+#             # Break the loop if the end of the video is reached
+#             break
 
-    # Close the FFMPEG process
-    ffmpeg_process.stdin.close()
-    ffmpeg_process.wait()
+#     # Close the FFMPEG process
+#     ffmpeg_process.stdin.close()
+#     ffmpeg_process.wait()
 
-    # Release the video capture object
-    cap.release()
+#     # Release the video capture object
+#     cap.release()
 
-    return output_video_path
+#     return output_video_path
 
 
-def process_video_with_yolo(video_path, target_class=[3]):
-    # Load the YOLO model
-    model = YOLO("models/best1.pt")
+# def process_video_with_yolo(video_path, target_class=[3]):
+#     # Load the YOLO model
+#     model = YOLO("models/best1.pt")
 
-    # Open the video file
-    cap = cv2.VideoCapture(video_path)
+#     # Open the video file
+#     cap = cv2.VideoCapture(video_path)
 
-    # Get video properties for the output video
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
+#     # Get video properties for the output video
+#     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+#     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#     fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-    # Set up FFMPEG process for writing the video
-    file_name_with_extension = os.path.basename(video_path)
-    file_name, file_extension = os.path.splitext(file_name_with_extension)
-    output_video_path = f"videos/result/{file_name}_output.mp4"
+#     # Set up FFMPEG process for writing the video
+#     file_name_with_extension = os.path.basename(video_path)
+#     file_name, file_extension = os.path.splitext(file_name_with_extension)
+#     output_video_path = f"videos/result/{file_name}_output.mp4"
 
-    ffmpeg_cmd = [
-        'ffmpeg', '-y',  # Overwrite if file exists
-        '-f', 'rawvideo', '-vcodec', 'rawvideo', '-pix_fmt', 'bgr24',
-        '-s', f'{frame_width}x{frame_height}', '-r', str(fps),
-        '-i', '-', '-c:v', 'libx264', '-preset', 'fast', '-pix_fmt', 'yuv420p',
-        output_video_path
-    ]
-    ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
+#     ffmpeg_cmd = [
+#         'ffmpeg', '-y',  # Overwrite if file exists
+#         '-f', 'rawvideo', '-vcodec', 'rawvideo', '-pix_fmt', 'bgr24',
+#         '-s', f'{frame_width}x{frame_height}', '-r', str(fps),
+#         '-i', '-', '-c:v', 'libx264', '-preset', 'fast', '-pix_fmt', 'yuv420p',
+#         output_video_path
+#     ]
+#     ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
 
-    # Loop through the video frames
-    while cap.isOpened():
-        # Read a frame from the video
-        success, frame = cap.read()
+#     # Loop through the video frames
+#     while cap.isOpened():
+#         # Read a frame from the video
+#         success, frame = cap.read()
 
-        if success:
-            # Run YOLO inference on the frame
-            results = model(frame)
+#         if success:
+#             # Run YOLO inference on the frame
+#             results = model(frame)
             
-            names = results[0].names
-            boxes = results[0].boxes
-            xyxys = boxes.xyxy
-            cls = boxes.cls
+#             names = results[0].names
+#             boxes = results[0].boxes
+#             xyxys = boxes.xyxy
+#             cls = boxes.cls
 
-            for cla, xyxy in zip(cls, xyxys):
-                cla_key = int(cla.item())
-                tensor_list = xyxy.tolist()
-                int_list = [int(element) for element in tensor_list]
+#             for cla, xyxy in zip(cls, xyxys):
+#                 cla_key = int(cla.item())
+#                 tensor_list = xyxy.tolist()
+#                 int_list = [int(element) for element in tensor_list]
 
-                # Check if the detection class matches target_class
-                if cla_key in target_class:
-                    # Extract bounding box coordinates
-                    x1, y1, x2, y2 = int_list
-                    label = names[cla_key]
+#                 # Check if the detection class matches target_class
+#                 if cla_key in target_class:
+#                     # Extract bounding box coordinates
+#                     x1, y1, x2, y2 = int_list
+#                     label = names[cla_key]
 
-                    # Draw the bounding box and label on the frame
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                    cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+#                     # Draw the bounding box and label on the frame
+#                     cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+#                     cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
 
-            # Write the processed frame to FFMPEG
-            ffmpeg_process.stdin.write(frame.tobytes())
-        else:
-            # Break the loop if the end of the video is reached
-            break
+#             # Write the processed frame to FFMPEG
+#             ffmpeg_process.stdin.write(frame.tobytes())
+#         else:
+#             # Break the loop if the end of the video is reached
+#             break
 
-    # Close the FFMPEG process
-    ffmpeg_process.stdin.close()
-    ffmpeg_process.wait()
+#     # Close the FFMPEG process
+#     ffmpeg_process.stdin.close()
+#     ffmpeg_process.wait()
 
-    # Release the video capture object
-    cap.release()
-    os.remove(video_path)
+#     # Release the video capture object
+#     cap.release()
+#     os.remove(video_path)
 
-    return output_video_path
+#     return output_video_path
 
 
 def send_image_to_gpt4(api_key, image_paths, prompt):
